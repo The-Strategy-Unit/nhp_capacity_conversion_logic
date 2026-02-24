e72b8e757b8ad21e2e6a19061406090dd7795cc7
YiWen Hon <yiwen.hon1@nhs.net>
2026-02-24 10:07:56 +0000

    Combine adult and child resus. Closes #8

--- src/nhp/capacity_conversion/aae.py
+++ src/nhp/capacity_conversion/aae.py
@@ -153,8 +153,7 @@ def calculate_aae_capacity(
         "child_major_beds",
         "child_minor_bays",
         "sdec_spaces",
-        "adult_resus_beds",
-        "child_resus_beds",  # TODO: see #8
+        "resus_spaces",
     ]:
         results = {}
         assumed_los_mins = cast(
