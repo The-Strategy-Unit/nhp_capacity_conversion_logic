e92f3d3231c4314528f1e753eb701a69b3d3e368
YiWen Hon <yiwen.hon1@nhs.net>
2026-03-02 14:22:07 +0000

    add test for calculate_aae_capacity

--- tests/unit/test_aae.py
+++ tests/unit/test_aae.py
@@ -7,6 +7,7 @@ from nhp.capacity_conversion.aae import (
     process_aae,
     convert_aae_capacity,
     map_aae_capacity_to_functional_area,
+    calculate_aae_capacity,
 )
 
 
@@ -108,3 +109,68 @@ def test_map_aae_capacity_to_functional_area():
     actual = map_aae_capacity_to_functional_area(capacity_requirement_string)
     # assert
     assert actual == expected
+
+
+def test_calculate_aae_capacity(mocker, caplog):
+    # arrange
+    caplog.set_level("INFO")
+
+    capacity_requirements = [
+        "adult_major_spaces",
+        "adult_minor_spaces",
+        "child_major_spaces",
+        "child_minor_spaces",
+        "sdec_spaces",
+        "resus_spaces",
+    ]
+    mocker.patch(
+        "nhp.capacity_conversion.aae.map_aae_capacity_to_functional_area",
+        return_value="mock_functional_area",
+    )
+    mock_convert = mocker.patch(
+        "nhp.capacity_conversion.aae.convert_aae_capacity",
+        return_value=999,
+    )
+    functional_areas_summarised = {
+        "mock_functional_area": {
+            "p10": 100,
+            "mean": 200,
+            "p90": 300,
+        }
+    }
+    assumptions_data = {}
+    for req in capacity_requirements:
+        assumptions_data[f"{req}_assumed_los"] = {"assumption_value": 240}
+        assumptions_data[f"{req}_operating_hours"] = {"assumption_value": 168}
+        assumptions_data[f"{req}_operating_weeks"] = {"assumption_value": 52}
+        assumptions_data[f"{req}_utilisation"] = {"assumption_value": 0.5}
+
+    assumptions_df = pd.DataFrame.from_dict(assumptions_data, orient="index")
+
+    # act
+    result = calculate_aae_capacity(
+        functional_areas_summarised,
+        assumptions_df,
+    )
+
+    # assert
+
+    # convert_aae_capacity should be called 18 times (6 × 3)
+    assert mock_convert.call_count == 18
+
+    # output structure
+    assert isinstance(result, pd.DataFrame)
+    assert list(result.columns) == ["p10", "mean", "p90"]
+    assert list(result.index) == capacity_requirements
+
+    # all values should be mocked return value
+    assert (result == 999).all().all()
+
+    # check arguments in calls to convert_aae_capacity
+    first_call = mock_convert.call_args_list[0]
+    args = first_call.args
+
+    assert args[1] == 240  # assumed_los
+    assert args[2] == 52  # operating_weeks
+    assert args[3] == 168  # operating_hours
+    assert args[4] == 0.5  # utilisation
