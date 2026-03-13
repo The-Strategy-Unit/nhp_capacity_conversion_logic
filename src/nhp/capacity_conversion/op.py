1192c73a55c53c4c81015cd8db64503df15330d1
YiWen Hon <yiwen.hon1@nhs.net>
2026-03-13 14:58:55 +0000

    finalise OP data pipeline

--- src/nhp/capacity_conversion/op.py
+++ src/nhp/capacity_conversion/op.py
@@ -7,19 +7,112 @@ from nhp.capacity_conversion.utils import (
     load_assumptions,
     summarise_functional_areas,
     save_results_to_excel,
-    calculate_prediction_intervals_and_mean,
     load_metadata_from_ats,
     create_aggregations_path,
     validate_required_env_vars,
     load_aggregations,
 )
 import argparse
+import re
 from typing import cast
 import sys
 from logging import INFO
 from datetime import datetime
 
 logger = get_logger()
+
+
+def convert_op_capacity(
+    attendances: float,
+    duration: float,
+    dna_rate: float,
+    dna_time: float,
+    operational_hours: float,
+    operational_weeks: float,
+    utilisation_rate: float,
+) -> float:
+    return (((attendances * duration) + (dna_rate * attendances * dna_time)) / 60) / (
+        operational_hours * operational_weeks * utilisation_rate
+    )
+
+
+def map_op_capacity_to_functional_area(capacity_requirement_string: str) -> str:
+    capacity_requirement_string = capacity_requirement_string.replace(
+        "op_", "outpatient_"
+    )
+    if "procedures" not in capacity_requirement_string:
+        capacity_requirement_string += "_attendances"
+
+    return capacity_requirement_string
+
+
+def calculate_op_capacity(
+    functional_areas_summarised: dict, assumptions_df: pd.DataFrame
+) -> pd.DataFrame:
+    logger.info("Calculating OP capacity")
+    results_dict = {}
+    for capacity_requirement in [
+        "op_first",
+        "op_followup",
+        "op_virtual",
+        "op_procedures",
+    ]:
+        results = {}
+        duration = cast(
+            float,
+            assumptions_df.at[
+                capacity_requirement + "_duration_mins", "assumption_value"
+            ],
+        )
+        dna_rate = cast(
+            float,
+            assumptions_df.at[capacity_requirement + "_dna_rate", "assumption_value"],
+        )
+        dna_time = cast(
+            float,
+            assumptions_df.at[
+                capacity_requirement + "_dna_time_mins", "assumption_value"
+            ],
+        )
+        operational_hours = cast(
+            float,
+            assumptions_df.at[
+                capacity_requirement + "_operational_hours", "assumption_value"
+            ],
+        )
+        operational_weeks = cast(
+            float,
+            assumptions_df.at[
+                capacity_requirement + "_operational_weeks", "assumption_value"
+            ],
+        )
+        utilisation_rate = cast(
+            float,
+            assumptions_df.at[
+                capacity_requirement + "_utilisation_rate", "assumption_value"
+            ],
+        )
+        for value in ["p10", "mean", "p90"]:
+            functional_area = map_op_capacity_to_functional_area(capacity_requirement)
+            results[value] = convert_op_capacity(
+                functional_areas_summarised[functional_area][value],
+                duration,
+                dna_rate,
+                dna_time,
+                operational_hours,
+                operational_weeks,
+                utilisation_rate,
+            )
+        results_dict[capacity_requirement] = results
+    op_capacity = pd.DataFrame.from_dict(results_dict, orient="index")
+    rename_dict = {
+        "op_virtual": "op_virtual_consultation_rooms",
+        "op_procedures": "op_procedure_rooms",
+    }
+    op_capacity.loc["op_consultation_rooms"] = (
+        op_capacity.loc["op_first"] + op_capacity.loc["op_followup"]
+    )
+    return op_capacity.rename(index=rename_dict)
 
 
 def main():
@@ -66,13 +159,12 @@ def main():
         config["AZ_STORAGE_EP"], config["AZ_STORAGE_RESULTS"], aggregations_path, "op"
     )
     functional_areas_summarised = summarise_functional_areas(op_aggregations)
-    return functional_areas_summarised
-    # data_to_save["op_functional_areas"] = pd.DataFrame.from_dict(
-    #     functional_areas_summarised, orient="index"
-    # )
-    # op_capacity_df = calculate_op_capacity(functional_areas_summarised, assumptions)
-    # data_to_save["op_capacity"] = op_capacity_df
-    # save_results_to_excel(data_to_save)
+    data_to_save["op_functional_areas"] = pd.DataFrame.from_dict(
+        functional_areas_summarised, orient="index"
+    )
+    op_capacity_df = calculate_op_capacity(functional_areas_summarised, assumptions)
+    data_to_save["op_capacity"] = op_capacity_df
+    save_results_to_excel(data_to_save)
 
 
 if __name__ == "__main__":
