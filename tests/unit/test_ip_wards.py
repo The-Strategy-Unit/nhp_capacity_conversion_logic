45ab535e13d5bf464b2a6e2ec5b6961a16510492
Hon YiWen (MLCSU) <yiwen.hon1@nhs.net>
2026-04-07 13:55:41 +0100

    add test for ip_wards main function

--- tests/unit/test_ip_wards.py
+++ tests/unit/test_ip_wards.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from nhp.capacity_conversion.ip_wards import (
     calculate_0los_beddays,
@@ -11,6 +11,7 @@ from nhp.capacity_conversion.ip_wards import (
     calculate_ward_beddays,
     convert_ip_beddays_to_beds,
     group_bedday_pools,
+    main,
 )
 
 
@@ -295,3 +296,118 @@ def test_calculate_ip_wards_capacity(mocker):
     mock_group.assert_called_once_with(bedday_pools)
     assert mock_convert.call_count == 9
     assert_frame_equal(actual, expected)
+
+
+def test_main(mocker):
+    # arrange
+    module_path = "nhp.capacity_conversion.ip_wards"
+
+    mock_now = mocker.Mock()
+    mock_now.strftime.return_value = "20250101_120000"
+    mocker.patch(f"{module_path}.datetime").now.return_value = mock_now
+
+    mock_parser = mocker.Mock()
+    mock_args = mocker.Mock()
+    mock_args.guid = "GUID123"
+    mock_args.path_to_assumptions_file = "assumptions.csv"
+    mock_args.capacity_model_version = "dev"
+
+    mock_parser.parse_args.return_value = mock_args
+    mocker.patch(f"{module_path}.argparse.ArgumentParser", return_value=mock_parser)
+    env_vars_dict = {
+        "AZ_STORAGE_EP": "AZ_STORAGE_EP",
+        "AZ_STORAGE_RESULTS": "AZ_STORAGE_RESULTS",
+        "TABLE_NAME": "TABLE_NAME",
+        "AZ_TABLE_ENDPOINT": "AZ_TABLE_ENDPOINT",
+    }
+    mocker.patch(
+        f"{module_path}.validate_required_env_vars", return_value=env_vars_dict
+    )
+    metadata_dict = {
+        "PartitionKey": "PartitionKey",
+        "RowKey": "RowKey",
+        "guid": "GUID123",
+        "capacity_model_version": "dev",
+    }
+    mocker.patch(
+        f"{module_path}.load_metadata_from_ats",
+        return_value=metadata_dict,
+    )
+    mocker.patch(
+        f"{module_path}.create_aggregations_path", return_value="aggregations_path"
+    )
+
+    mock_assumptions = pd.DataFrame()
+    mocker.patch(f"{module_path}.load_assumptions", return_value=mock_assumptions)
+
+    mock_aggregations = pd.DataFrame(
+        {
+            "grouping": ["a", "b", "c"] * 3,
+            "model_run": [0] * 3 + [1] * 3 + [2] * 3,
+            "total": [3] * 3 + [4] * 3 + [5] * 3,
+        }
+    )
+    mocker.patch(f"{module_path}.load_aggregations", return_value=mock_aggregations)
+
+    mock_functional_summary = {"area": {"mean": 1}}
+    mocker.patch(
+        f"{module_path}.summarise_functional_areas",
+        return_value=mock_functional_summary,
+    )
+
+    mocker.patch(
+        f"{module_path}.calculate_separate_bedday_pools",
+        return_value=mock_functional_summary,
+    )
+
+    mock_capacity_df = pd.DataFrame({"mean": [1]})
+    mocker.patch(
+        f"{module_path}.calculate_ip_wards_capacity",
+        return_value=mock_capacity_df,
+    )
+
+    mock_save = mocker.patch(f"{module_path}.save_results_to_excel")
+
+    # act
+
+    main()
+
+    # assert
+
+    module = __import__(module_path, fromlist=["dummy"])
+    module.load_metadata_from_ats.assert_called_once_with(
+        "GUID123", "AZ_TABLE_ENDPOINT", "TABLE_NAME", "dev"
+    )
+    module.load_assumptions.assert_called_once_with("assumptions.csv")
+    module.create_aggregations_path.assert_called_once_with(metadata_dict)
+    module.load_aggregations.assert_called_once_with(
+        "AZ_STORAGE_EP", "AZ_STORAGE_RESULTS", "aggregations_path", "ip_wards"
+    )
+    module.summarise_functional_areas.assert_called_once_with(mock_aggregations)
+    module.calculate_separate_bedday_pools.assert_called_once_with(
+        mock_functional_summary,
+        mock_assumptions,
+    )
+    module.calculate_ip_wards_capacity.assert_called_once_with(
+        mock_functional_summary,
+        mock_assumptions,
+    )
+    mock_save.assert_called_once()
+    mock_data_to_save = mock_save.call_args_list[0].args[0]
+    assert_series_equal(
+        mock_data_to_save["metadata"],
+        pd.Series(
+            {
+                "guid": "GUID123",
+                "capacity_model_version": "dev",
+                "capacity_conversion_runtime": "20250101_120000",
+            }
+        ),
+    )
+    assert_frame_equal(mock_data_to_save["assumptions"], pd.DataFrame())
+    assert_frame_equal(
+        mock_data_to_save["ip_wards_functional_areas"],
+        pd.DataFrame.from_dict(mock_functional_summary, orient="index"),
+    )
+    assert mock_data_to_save["calculated_bedday_pools"] == mock_functional_summary
+    assert_frame_equal(mock_data_to_save["ip_wards_capacity"], mock_capacity_df)
