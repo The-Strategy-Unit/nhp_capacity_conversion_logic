36b9435cd39bd19d7d751e049e435dba35222e8d
Hon YiWen (MLCSU) <yiwen.hon1@nhs.net>
2026-06-23 17:11:14 +0100

    remove old assumptions file, store URL of new file in config

--- src/nhp/capacity_conversion/utils.py
+++ src/nhp/capacity_conversion/utils.py
@@ -60,8 +60,8 @@ def calculate_prediction_intervals_and_mean(
 
 
 def load_assumptions(path_to_csv: str) -> pd.DataFrame:
-    """Loads assumptions for use in model
-    TODO: #7 Currently loads from CSV but later we should allow users to set in another way
+    """Loads assumptions for use in model. Defaults to assumptions published online at
+    https://the-strategy-unit.github.io/open-plan-docs/reference/functional-area-catalogue/
 
     Args:
         path_to_csv (str): Path to assumptions csv file
@@ -70,7 +70,7 @@ def load_assumptions(path_to_csv: str) -> pd.DataFrame:
         pd.DataFrame: Dataframe with assumption values and variable names
     """
     logger.info(f"Loading assumptions from {path_to_csv}...")
-    return pd.read_csv(path_to_csv).set_index("assumption_name").sort_index()
+    return pd.read_csv(path_to_csv).set_index("Assumption ID")[["Value"]].sort_index()
 
 
 def save_results_to_excel(data_to_save: dict[str, pd.DataFrame | pd.Series]) -> None:
