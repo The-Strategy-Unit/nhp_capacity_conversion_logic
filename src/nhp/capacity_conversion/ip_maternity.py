7e695c23297c8f45997bdc5d28990b7c88078b89
Hon YiWen (MLCSU) <yiwen.hon1@nhs.net>
2026-07-22 11:32:10 +0100

    add theatres obstetric proc logic

--- src/nhp/capacity_conversion/ip_maternity.py
+++ src/nhp/capacity_conversion/ip_maternity.py
@@ -8,7 +8,9 @@ from nhpy.utils import get_logger
 
 from nhp.capacity_conversion.ip_formulas import (
     calculate_recovery_capacity,
+    calculate_time_util_capacity,
     derive_recovery_occupancy_hours,
+    derive_treatment_hours,
 )
 from nhp.capacity_conversion.utils import run_single_activity_type
 
@@ -17,9 +19,101 @@ logger = get_logger()
 
 @dataclass(frozen=True)
 class MaternityConfig:
+    subgroup: str
     col_to_use: str
     formula: Callable
     assumptions: dict[str, str]
+
+
+def process_theatres_obstetric_proc_data(
+    functional_areas: pd.DataFrame,
+) -> pd.DataFrame:
+    """Combine activity for elective and nonelective csections to create obstetric_theatre_procedures
+    functional area grouping for calculation of theatres for obstetric procedures
+
+    Args:
+        functional_areas (pd.DataFrame): Functional areas from Azure for IP maternity
+
+    Returns:
+        pd.DataFrame: IP maternity functional areas with new grouping obstetric_theatre_procedures
+    """
+    obstetric_theatre_procedures = (
+        functional_areas[
+            functional_areas["grouping"].isin(
+                [
+                    "maternity_elective_csection_nonzerolos",
+                    "maternity_nonelective_csection_nonzerolos",
+                    "maternity_elective_csection_zerolos",
+                    "maternity_nonelective_csection_zerolos",
+                ]
+            )
+        ]
+        .groupby(level=0)
+        .sum()
+        .assign(grouping="obstetric_theatre_procedures")
+    )
+    result = pd.concat([functional_areas, obstetric_theatre_procedures]).sort_index()
+    return result
+
+
+def preprocess_ip_maternity_data(functional_areas: pd.DataFrame) -> pd.DataFrame:
+    """Preprocesses IP maternity data for conversion to capacity.
+
+    Args:
+        functional_areas (pd.DataFrame): Functional areas from Azure for IP maternity
+
+    Returns:
+        pd.DataFrame: Preprocessed IP maternity data for conversion to capacity
+    """
+    functional_areas_processed = process_theatres_obstetric_proc_data(functional_areas)
+    return functional_areas_processed
+
+
+def calculate_theatres_obstetric_proc(
+    subgroup: str,
+    assumptions: dict[str, str],
+    functional_area_subgroup: pd.Series,
+    assumptions_df: pd.DataFrame,
+) -> pd.DataFrame:
+    """Calculates capacity requirements for obstetric theatres using the FRM_TIME_UTIL conversion archetype
+
+    Args:
+        subgroup (str): Name of functional area subgroup
+        assumptions (dict[str, str]): Mapping of assumption name to use for the specific subgroup
+        functional_area_subgroup (pd.Series): Functional area groupings in a Pandas Series, with the index name model_run
+        assumptions_df (pd.DataFrame): DataFrame with required assumptions for calculating capacity
+
+    Returns:
+        pd.DataFrame: Calculated capacity requirements for the specific subgroup
+    """
+
+    time = cast(float, assumptions_df.at[assumptions["treatment_time"], "Value"])
+    utilisation = cast(
+        float,
+        assumptions_df.at[assumptions["treatment_utilisation"], "Value"],
+    )
+    annual_operational_hours = cast(
+        float,
+        assumptions_df.at[
+            assumptions["treatment_annual_operational_hours"],
+            "Value",
+        ],
+    )
+    output = "OBSTETRIC_PROC_THEATRES"
+    treatment_hours = derive_treatment_hours(
+        time,
+        functional_area_subgroup,
+    )
+    results = pd.DataFrame(
+        calculate_time_util_capacity(
+            treatment_hours,
+            annual_operational_hours,
+            utilisation,
+        )
+    )
+    results.loc[:, "output"] = output
+    results = results.reset_index().set_index(["output", "model_run"])
+    return results
 
 
 def calculate_maternity_assessment_beds(
@@ -64,17 +158,26 @@ def calculate_maternity_assessment_beds(
 
 
 MATERNITY_CONFIG = {
-    "MATERNITY_ASSESSMENT_BEDS": {
-        "maternity_assessment": MaternityConfig(
-            col_to_use="spells",
-            formula=calculate_maternity_assessment_beds,
-            assumptions={
-                "recovery_time": "MATERNITY_ASSESSMENT_ZERO_DAY_LOS",
-                "recovery_occupancy": "MATERNITY_ASSESSMENT_OCC",
-                "recovery_annual_operational_hours": "MATERNITY_ASSESSMENT_ANNUAL_OPERATIONAL_HOURS",
-            },
-        )
-    }
+    "MATERNITY_ASSESSMENT_BEDS": MaternityConfig(
+        subgroup="maternity_assessment",
+        col_to_use="spells",
+        formula=calculate_maternity_assessment_beds,
+        assumptions={
+            "recovery_time": "MATERNITY_ASSESSMENT_ZERO_DAY_LOS",
+            "recovery_occupancy": "MATERNITY_ASSESSMENT_OCC",
+            "recovery_annual_operational_hours": "MATERNITY_ASSESSMENT_ANNUAL_OPERATIONAL_HOURS",
+        },
+    ),
+    "OBSTETRIC_PROC_THEATRES": MaternityConfig(
+        subgroup="obstetric_theatre_procedures",
+        col_to_use="spells",
+        formula=calculate_theatres_obstetric_proc,
+        assumptions={
+            "treatment_time": "OBSTETRIC_THEATRE_PROC_TIME",
+            "treatment_utilisation": "OBSTETRIC_THEATRE_UTIL",
+            "treatment_annual_operational_hours": "OBSTETRIC_THEATRE_ANNUAL_OPERATIONAL_HOURS",
+        },
+    ),
 }
 
 
@@ -94,19 +197,18 @@ def calculate_maternity_capacity(
     """
     logger.info("Calculating IP maternity capacity")
     results_list = []
-    for output, output_config in config.items():
-        for subgroup, subgroup_config in output_config.items():
-            functional_area_subgroup = functional_areas.xs(
-                key=subgroup, level="grouping"
-            )[subgroup_config.col_to_use]
-            results_list.append(
-                subgroup_config.formula(
-                    subgroup=subgroup,
-                    assumptions=subgroup_config.assumptions,
-                    functional_area_subgroup=functional_area_subgroup,
-                    assumptions_df=assumptions_df,
-                )
+    for output, subgroup_config in config.items():
+        functional_area_subgroup = functional_areas.xs(
+            key=subgroup_config.subgroup, level="grouping"
+        )[subgroup_config.col_to_use]
+        results_list.append(
+            subgroup_config.formula(
+                subgroup=subgroup_config.subgroup,
+                assumptions=subgroup_config.assumptions,
+                functional_area_subgroup=functional_area_subgroup,
+                assumptions_df=assumptions_df,
             )
+        )
     return pd.concat(results_list)
 
 
@@ -117,7 +219,11 @@ def main():
     Returns:
         int: Exit code (0 for success, 2 for errors)
     """
-    return run_single_activity_type("ip_maternity", calculate_maternity_capacity)
+    return run_single_activity_type(
+        "ip_maternity",
+        calculate_maternity_capacity,
+        preprocess=preprocess_ip_maternity_data,
+    )
 
 
 if __name__ == "__main__":
